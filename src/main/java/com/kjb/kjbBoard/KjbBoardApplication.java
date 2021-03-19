38aaaefb3b0e3d4793c6cc8c3bd0ba3c73efd282
kjb9027 <kjb9027@naver.com>
2021-03-19 18:14:53 +0900

    처음 커밋

---
+++ src/main/java/com/kjb/kjbBoard/KjbBoardApplication.java
@@ -0,0 +1,13 @@
+package com.kjb.kjbBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KjbBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KjbBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kjb/kjbBoard/KjbBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjb.kjbBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KjbBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
