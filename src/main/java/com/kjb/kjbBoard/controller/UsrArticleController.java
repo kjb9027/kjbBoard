0f165b021310a4497edeea3b6b700bc0186a52e4
kjb9027 <kjb9027@naver.com>
2021-03-22 15:12:45 +0900

    게시글 작성
    임의의 데이터 인서트

--- src/main/java/com/kjb/kjbBoard/controller/UsrArticleController.java
+++ src/main/java/com/kjb/kjbBoard/controller/UsrArticleController.java
@@ -1,7 +1,9 @@
 package com.kjb.kjbBoard.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,18 @@ import com.kjb.kjbBoard.dto.Article;
 
 @Controller
 public class UsrArticleController {
+	private int articlesLastId;
 	private List<Article> articles;
-	
+
 	public UsrArticleController() {
-		articles=new ArrayList<>();
-		articles.add(new Article(1, "2020-12-12 12:12:12", "제목1", "내용1"));
-		articles.add(new Article(2, "2020-12-13 12:12:12", "제목2", "내용2"));
+		// 맴버 변수 초기화
+		articlesLastId=0;
+		articles = new ArrayList<>();
+		// 게시물 두개 생성
+		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목1", "내용1"));
+		articles.add(new Article(++articlesLastId, "2020-12-13 12:12:12", "제목2", "내용2"));
 	}
+
 	@RequestMapping("user/article/detail")
 	@ResponseBody
 	public Article showDetail(int id) {
@@ -28,5 +35,16 @@ public class UsrArticleController {
 	@ResponseBody
 	public List<Article> showList() {
 		return articles;
+	}
+
+	@RequestMapping("user/article/doAdd")
+	@ResponseBody
+	public Map<String, Object> doAdd(String regDate, String title, String body) {
+		articles.add(new Article(++articlesLastId, regDate, title, body));
+		Map<String,Object> rs = new HashMap<>();
+		rs.put("resultCode", "S-1");
+		rs.put("msg", "성공하였습니다");
+		rs.put("id", articlesLastId);
+		return rs;
 	}
 }
