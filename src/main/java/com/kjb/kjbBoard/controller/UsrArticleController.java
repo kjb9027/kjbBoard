2d8999f719ba02001aa37615d9e55430dcf77b2b
kjb9027 <kjb9027@naver.com>
2021-03-22 15:28:55 +0900

    게시글 삭제
    임의의 데이터 삭제

--- src/main/java/com/kjb/kjbBoard/controller/UsrArticleController.java
+++ src/main/java/com/kjb/kjbBoard/controller/UsrArticleController.java
@@ -20,9 +20,12 @@ public class UsrArticleController {
 		// 맴버 변수 초기화
 		articlesLastId=0;
 		articles = new ArrayList<>();
-		// 게시물 두개 생성
+		// 게시물 다섯개 생성
 		articles.add(new Article(++articlesLastId, "2020-12-12 12:12:12", "제목1", "내용1"));
 		articles.add(new Article(++articlesLastId, "2020-12-13 12:12:12", "제목2", "내용2"));
+		articles.add(new Article(++articlesLastId, "2020-12-14 12:12:12", "제목3", "내용3"));
+		articles.add(new Article(++articlesLastId, "2020-12-15 12:12:12", "제목4", "내용4"));
+		articles.add(new Article(++articlesLastId, "2020-12-16 12:12:12", "제목5", "내용5"));
 	}
 
 	@RequestMapping("user/article/detail")
@@ -46,5 +49,29 @@ public class UsrArticleController {
 		rs.put("msg", "성공하였습니다");
 		rs.put("id", articlesLastId);
 		return rs;
+	}
+	@RequestMapping("user/article/doDelete")
+	@ResponseBody
+	public Map<String, Object> doDelete(int id) {
+		boolean deleteArticleRs = deleteArticle(id);
+		Map<String,Object> rs = new HashMap<>();
+		if(deleteArticleRs) {
+			rs.put("resultCode", "S-1");
+			rs.put("msg", "성공하였습니다");
+		}else {
+			rs.put("resultCode", "F-1");
+			rs.put("msg", "해당게시글은 존재하지않습니다");
+		}
+		return rs;
+	}
+
+	private boolean deleteArticle(int id) {
+		for(Article article:articles) {
+			if(article.getId()==id) {
+				articles.remove(article);
+				return true;
+			}
+		}
+		return false;
 	}
 }
