01db1339fcf684e051865965511dfaec4caba5c0
kjb9027 <kjb9027@naver.com>
2021-03-25 15:12:07 +0900

    로그인 기능
    입력한 아이디와 패스워드가 일치하면 세션에 아이디 저장 후 로그인 되게 설정
    세션에 이미 아이디가 있다면 로그인이 또 되지 않게 설정

--- src/main/java/com/kjb/kjbBoard/controller/UsrMemberController.java
+++ src/main/java/com/kjb/kjbBoard/controller/UsrMemberController.java
@@ -2,6 +2,8 @@ package com.kjb.kjbBoard.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +22,10 @@ public class UsrMemberController {
 	@ResponseBody
 	public ResultData doJoin(@RequestParam Map<String,Object> param) {
 		return memberService.joinMember(param);
+	}
+	@RequestMapping("user/member/doLogin")
+	@ResponseBody
+	public ResultData doLogin(@RequestParam Map<String,Object> param, HttpSession session) {
+		return memberService.loginMember(param,session);
 	}
 }
--- src/main/java/com/kjb/kjbBoard/service/MemberService.java
+++ src/main/java/com/kjb/kjbBoard/service/MemberService.java
@@ -2,6 +2,8 @@ package com.kjb.kjbBoard.service;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,38 +11,60 @@ import com.kjb.kjbBoard.dao.MemberDao;
 import com.kjb.kjbBoard.dto.Member;
 import com.kjb.kjbBoard.dto.ResultData;
 import com.kjb.kjbBoard.util.Util;
+
 @Service
 public class MemberService {
 	@Autowired
 	private MemberDao memberDao;
 
 	public ResultData joinMember(Map<String, Object> param) {
-		Member existMember = memberDao.getMember(param.get("loginId").toString());
-		if(param.get("loginId")==null) {
+		if (param.get("loginId") == null) {
 			return new ResultData("F-1", "id을 입력해주세요.");
 		}
-		if(existMember != null) {
+		Member existMember = memberDao.getMember(param.get("loginId").toString());
+		if (existMember != null) {
 			return new ResultData("F-2", String.format("%s (은)는 이미 사용중인 아이디 입니다.", existMember.getId()));
 		}
-		if(param.get("loginPw")==null) {
+		if (param.get("loginPw") == null) {
 			return new ResultData("F-1", "pw를 입력해주세요.");
 		}
-		if(param.get("name")==null) {
+		if (param.get("name") == null) {
 			return new ResultData("F-1", "name을 입력해주세요.");
 		}
-		if(param.get("nickName")==null) {
+		if (param.get("nickName") == null) {
 			return new ResultData("F-1", "nickName를 입력해주세요.");
 		}
-		if(param.get("phoneNo")==null) {
+		if (param.get("phoneNo") == null) {
 			return new ResultData("F-1", "phoneNo을 입력해주세요.");
 		}
-		if(param.get("email")==null) {
+		if (param.get("email") == null) {
 			return new ResultData("F-1", "email를 입력해주세요.");
 		}
 		memberDao.joinMember(param);
-		int id = Util.getAsInt(param.get("id"),0);
+		int id = Util.getAsInt(param.get("id"), 0);
 		System.out.println(param.get("nickName"));
 		return new ResultData("S-1", String.format("%s님 환영합니다", param.get("nickName")), "id", id);
+	}
+
+	public ResultData loginMember(Map<String, Object> param, HttpSession session) {
+		if (session.getAttribute("loginedMemberId") != null) {
+			return new ResultData("F-4", "이미 로그인되어있습니다.");
+		}
+		if (param.get("loginId") == null) {
+			return new ResultData("F-1", "id을 입력해주세요.");
+		}
+		if (param.get("loginPw") == null) {
+			return new ResultData("F-1", "pw를 입력해주세요.");
+		}
+		Member existMember = memberDao.getMember(param.get("loginId").toString());
+		if (existMember == null) {
+			return new ResultData("F-2", "존재하지 않는 로그인아이디 입니다.", "loginId", param.get("loginId"));
+		}
+		if (existMember.getLoginPw().equals(param.get("loginPw")) == false) {
+			return new ResultData("F-3", "비밀번호가 일치하지 않습니다.");
+		}
+		session.setAttribute("loginedMemberId", existMember.getId());
+		return new ResultData("S-1", String.format("%s님 환영합니다", existMember.getNickName()));
 	}
 
 }
